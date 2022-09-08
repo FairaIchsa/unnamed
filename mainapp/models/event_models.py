e18d2bc2d0b50fc6e85a3131fd774265a02b9fad
FairaIchsa <zelov02@yandex.ru>
2022-09-08 22:56:59 +0300

    added event model

---
+++ mainapp/models/event_models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField(max_length=1023)
