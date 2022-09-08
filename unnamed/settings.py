95b104900c1851744b737001147eab646af4d18a
FairaIchsa <zelov02@yandex.ru>
2022-09-08 23:15:15 +0300

    first migration

--- unnamed/settings.py
+++ unnamed/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import secret_settings
+from unnamed import secret_settings
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
