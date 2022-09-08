862a74345017539e90a21a21d3ac24f51f0a8430
FairaIchsa <zelov02@yandex.ru>
2022-09-08 23:27:37 +0300

    changed Event model

--- mainapp/models/event_models.py
+++ mainapp/models/event_models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    description = models.TextField(max_length=1023)
+    description = models.TextField(max_length=1023, blank=True, null=True)
