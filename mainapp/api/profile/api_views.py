7f480ddad3f30a298344554b925efeabe72dcf1d
FairaIchsa <zelov02@yandex.ru>
2023-04-16 02:23:29 +0300

    added search in profile query params

--- mainapp/api/profile/api_views.py
+++ mainapp/api/profile/api_views.py
@@ -4,8 +4,12 @@ from mainapp.models.user_models import User
 
 
 class UserListAPIView(generics.ListAPIView):
-    queryset = User.objects.all()
     serializer_class = UserListSerializer
+
+    def get_queryset(self):
+        search = self.request.query_params['search'] if 'search' in self.request.query_params else ''
+        queryset = User.objects.filter(name__contains=search) | User.objects.filter(email__contains=search)
+        return queryset
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
--- mainapp/api/profile/urls.py
+++ mainapp/api/profile/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-
 from .api_views import UserListAPIView, UserRetrieveAPIView
 
 app_name = 'profile'
 
 urlpatterns = [
-    path('all', UserListAPIView.as_view(), name='all'),
+    path('', UserListAPIView.as_view(), name='all'),
     path('<int:pk>', UserRetrieveAPIView.as_view(), name='by-id'),
+
+    # path('<int:pk>/follow', _.as_view(), name='follow'),
+    # path('<int:pk>/unfollow', _.as_view(), name='unfollow'),
 
     # path('get_user_data/', UserDataAPIView.as_view(), name='info'),
     # path('update_user_data/', UpdateUserDataAPIView.as_view(), name='update'),
