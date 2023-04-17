50769f59732ba60a8a37647e89e893a5842ac389
FairaIchsa <zelov02@yandex.ru>
2023-04-18 01:57:46 +0300

    added EventEditPermission

--- mainapp/api/event/api_views.py
+++ mainapp/api/event/api_views.py
@@ -1,22 +1,17 @@
 from rest_framework import generics, permissions, status, mixins, viewsets
 from rest_framework.response import Response
 from .serializers import EventCreateUpdateSerializer, EventListSerializer, EventRetrieveSerializer
+from .permissions import EventEditPermission
 from mainapp.models.event_models import Event
 
 
 class EventCreateUpdateViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.UpdateModelMixin):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [EventEditPermission]
     queryset = Event.objects.all()
     serializer_class = EventCreateUpdateSerializer
 
     def perform_create(self, serializer):
         serializer.save(host=self.request.user)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.host != self.request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN, data={'detail': 'Is not the host'})
-        super().update(request, *args, **kwargs)
 
 
 class EventListAPIView(generics.ListAPIView):
@@ -37,12 +32,5 @@ class EventRetrieveAPIView(generics.RetrieveAPIView):
 
 
 class EventDestroyAPIView(generics.DestroyAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [EventEditPermission]
     queryset = Event.objects.all()
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.host != self.request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN, data={'detail': 'Is not the host'})
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
---
+++ mainapp/api/event/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class EventEditPermission(permissions.BasePermission):
+    message = 'Only the host can edit events.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.host == request.user
