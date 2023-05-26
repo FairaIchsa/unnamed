4eb309a1f2088a902313aa1efe01503b4f2e38ad
FairaIchsa <zelov02@yandex.ru>
2023-05-26 22:26:13 +0300

    added EventReadSerializer parent model

--- api/event/serializers.py
+++ api/event/serializers.py
@@ -9,13 +9,8 @@ class EventCreateUpdateSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'category', 'location', 'description', 'time']
 
 
-class EventListSerializer(serializers.ModelSerializer):
+class EventReadSerializer(serializers.ModelSerializer):
     from api.user.serializers import UserListSerializer as UserSerializer
-
-    class Meta:
-        depth = 1
-        model = Event
-        fields = ['id', 'title', 'host', 'category', 'location', 'time', 'is_participating']
 
     host = UserSerializer()
     is_participating = serializers.SerializerMethodField()
@@ -27,7 +22,14 @@ class EventListSerializer(serializers.ModelSerializer):
         return event.participants.filter(pk=request.user.pk).exists()
 
 
-class EventRetrieveSerializer(serializers.ModelSerializer):
+class EventListSerializer(EventReadSerializer):
+    class Meta:
+        depth = 1
+        model = Event
+        fields = ['id', 'title', 'host', 'category', 'location', 'time', 'is_participating']
+
+
+class EventRetrieveSerializer(EventReadSerializer):
     from api.user.serializers import UserListSerializer
 
     class Meta:
@@ -36,12 +38,4 @@ class EventRetrieveSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'category', 'host', 'participants', 'location', 'description', 'time',
                   'is_participating']
 
-    host = UserListSerializer()
     participants = UserListSerializer(many=True)
-    is_participating = serializers.SerializerMethodField()
-
-    def get_is_participating(self, event):
-        request = self.context['request']
-        if not request.user.is_authenticated:
-            return False
-        return event.participants.filter(pk=request.user.pk).exists()
