b2032a95c3e90c020da6d5813be18b4342e1633b
FairaIchsa <zelov02@yandex.ru>
2023-05-28 17:25:49 +0300

    refactored code

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -14,6 +14,4 @@ urlpatterns = [
     path('get_full_user_data/', UserFullDataRetrieveAPIView.as_view(), name='get-full-user-data'),
     path('update_user_data/', UserDataUpdateAPIVIew.as_view(), name='update-user-data'),
     path('update_user_password/', UserPasswordUpdateAPIView.as_view(), name='update-user-password'),
-    # path('delete_user/', UserDestroyAPIView.as_view(), name='destroy-user'),
-    # path('logout/', LogOutAPIView.as_view(), name='logout'),
 ]
